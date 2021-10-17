5836ac7ca0dc5ef34e72d90e1c625e3cafe704e9
zryc1998 <zryc1998@gmail.com>
2021-10-17 13:14:31 +1300

    Initial commit

---
+++ app/src/main/java/com/example/workouttimer/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.workouttimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        final TextView timeTV = findViewById(R.id.time_text_view);
+        timeTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = View.inflate(MainActivity.this, R.layout.time_dialog, null);
+                final NumberPicker numberPickerHour = view.findViewById(R.id.numpicker_hours);
+                numberPickerHour.setMaxValue(23);
+                numberPickerHour.setValue(sharedPreferences.getInt("Hours", 0));
+                final NumberPicker numberPickerMinutes = view.findViewById(R.id.numpicker_minutes);
+                numberPickerMinutes.setMaxValue(59);
+                numberPickerMinutes.setValue(sharedPreferences.getInt("Minutes", 0));
+                final NumberPicker numberPickerSeconds = view.findViewById(R.id.numpicker_seconds);
+                numberPickerSeconds.setMaxValue(59);
+                numberPickerSeconds.setValue(sharedPreferences.getInt("Seconds", 0));
+                Button cancel = view.findViewById(R.id.cancel);
+                Button ok = view.findViewById(R.id.ok);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setView(view);
+                final AlertDialog alertDialog = builder.create();
+                cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.dismiss();
+                    }
+                });
+                ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        timeTV.setText(numberPickerHour.getValue() + ":" + numberPickerMinutes.getValue() + ":" + numberPickerSeconds.getValue());
+//                        timeTV.setText(String.format("%1$d:%2$02d:%3$02d", numberPickerHour.getValue(), numberPickerMinutes.getValue(), numberPickerSeconds.getValue()));
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("Hours", numberPickerHour.getValue());
+                        editor.putInt("Minutes", numberPickerMinutes.getValue());
+                        editor.putInt("Seconds", numberPickerSeconds.getValue());
+                        editor.apply();
+                        alertDialog.dismiss();
+                    }
+                });
+                alertDialog.show();
+            }
+        });
+    }
+}
