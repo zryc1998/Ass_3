eaf9a411495549acb4073946fbf03584fcd08fee
zryc1998 <zryc1998@gmail.com>
2021-10-17 14:54:16 +1300

    functional timepicker with basic layout and styles

--- app/src/main/java/com/example/workouttimer/MainActivity.java
+++ app/src/main/java/com/example/workouttimer/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.workouttimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AlertDialog;
+
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
@@ -16,21 +20,33 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTimePicker();
+    }
+
+    private void initTimePicker() {
+
         final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        final TextView timeTV = findViewById(R.id.time_text_view);
+        final EditText timeTV = findViewById(R.id.time_text_view);
+
+        timeTV.setRawInputType(InputType.TYPE_NULL);
         timeTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 View view = View.inflate(MainActivity.this, R.layout.time_dialog, null);
+
                 final NumberPicker numberPickerMinutes = view.findViewById(R.id.numpicker_minutes);
+                final NumberPicker numberPickerSeconds = view.findViewById(R.id.numpicker_seconds);
+
                 numberPickerMinutes.setMaxValue(59);
                 numberPickerMinutes.setValue(sharedPreferences.getInt("Minutes", 0));
-                final NumberPicker numberPickerSeconds = view.findViewById(R.id.numpicker_seconds);
                 numberPickerSeconds.setMaxValue(59);
                 numberPickerSeconds.setValue(sharedPreferences.getInt("Seconds", 0));
+
                 Button cancel = view.findViewById(R.id.cancel);
                 Button ok = view.findViewById(R.id.ok);
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
                 builder.setView(view);
                 final AlertDialog alertDialog = builder.create();
                 cancel.setOnClickListener(new View.OnClickListener() {
@@ -40,10 +56,10 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
                 ok.setOnClickListener(new View.OnClickListener() {
+                    @SuppressLint("DefaultLocale")
                     @Override
                     public void onClick(View v) {
-                        timeTV.setText(numberPickerMinutes.getValue() + ":" + numberPickerSeconds.getValue());
-//                        timeTV.setText(String.format("%1$d:%2$02d:%3$02d", numberPickerHour.getValue(), numberPickerMinutes.getValue(), numberPickerSeconds.getValue()));
+                        timeTV.setText(String.format("%02d:%02d", numberPickerMinutes.getValue(), numberPickerSeconds.getValue()));
                         SharedPreferences.Editor editor = sharedPreferences.edit();
                         editor.putInt("Minutes", numberPickerMinutes.getValue());
                         editor.putInt("Seconds", numberPickerSeconds.getValue());
