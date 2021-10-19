6a4f4441e9cdd5296f12418acf73f98d6a657c28
zryc1998 <zryc1998@gmail.com>
2021-10-19 14:09:11 +1300

    Most functions are working properly v2.

--- app/src/main/java/com/example/workouttimer/MainActivity.java
+++ app/src/main/java/com/example/workouttimer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.workouttimer;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -7,10 +8,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -18,13 +25,18 @@ import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
     ArrayList<TimerList> mTimerList = new ArrayList<>();
     RecyclerViewAdapter mAdapter;
+    Button mClearButton;
+    boolean mStopHandler;
+    boolean mIsAtTop;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,10 +48,83 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(linearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(linearLayoutManager);
-
         mAdapter = new RecyclerViewAdapter(mTimerList);
         mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View view, int i, int i1, int i2, int i3) {
+            if(!mIsAtTop)
+            {
+                mClearButton.setText("Back to top");
+                mClearButton.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FFAB00")));
+            }
+            }
+        });
 
+        initClearButton();
+
+    }
+
+    private void initClearButton() {
+     mClearButton = (Button) findViewById(R.id.clear_button);
+
+        mClearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mIsAtTop) {
+                    Objects.requireNonNull(mRecyclerView.getLayoutManager()).scrollToPosition(0);
+                    mIsAtTop = true;
+                    mClearButton.setText("Clear All");
+                    mClearButton.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#D50000")));
+                }
+                else deleteAllItems();
+            }
+        });
+    }
+
+
+    private void deleteAllItems() {
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (mTimerList.size() == 0) {
+                    mStopHandler = true;
+                }
+                if (!mStopHandler) {
+                    View v = Objects.requireNonNull(mRecyclerView.findViewHolderForAdapterPosition(0)).itemView;
+                    deleteItem(v, 0);
+                } else {
+                    handler.removeCallbacksAndMessages(null);
+                }
+                handler.postDelayed(this, 500);
+            }
+        };
+        this.runOnUiThread(runnable);
+    }
+
+    private void deleteItem(View rowView, int position) {
+        Animation anim = AnimationUtils.loadAnimation(rowView.getContext(),
+                android.R.anim.slide_out_right);
+        anim.setDuration(300);
+        rowView.startAnimation(anim);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                if (mTimerList.size() == 0) {
+//                    addEmptyView(); // adding empty view instead of the RecyclerView
+                    return;
+                }
+                mTimerList.remove(position);
+                mAdapter.notifyDataSetChanged();
+
+                if (mTimerList.size()==0) {
+                    mClearButton.setVisibility(View.INVISIBLE);
+                    recreate();
+                }
+            }
+
+        }, anim.getDuration());
     }
 
 
@@ -113,6 +198,9 @@ public class MainActivity extends AppCompatActivity {
                     mTimerList.add(tl);
                     mAdapter.setTimerList(mTimerList);
                     mAdapter.notifyDataSetChanged();
+                }
+                if (mTimerList.size()>0) {
+                    mClearButton.setVisibility(View.VISIBLE);
                 }
 //                test arraylist
 //                for (TimerList tl : mTimerList) {
--- app/src/main/java/com/example/workouttimer/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/workouttimer/RecyclerViewAdapter.java
@@ -1,29 +1,19 @@
 package com.example.workouttimer;
 
 import android.annotation.SuppressLint;
-import android.app.Dialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.media.MediaPlayer;
-import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.CountDownTimer;
-import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.PopupMenu;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.app.Activity;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.SimpleDateFormat;
@@ -36,6 +26,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     private final SimpleDateFormat mDateFormat;
     private View mView;
     boolean mIsRunning;
+    boolean mIsPause;
     private CountDownTimer mCountDownTimer;
 
 
@@ -65,37 +56,58 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.item_workout.setText(item.getName());
         holder.item_time.setText(mDateFormat.format(item.getTime()));
 
-
-        holder.itemView.setOnClickListener( new View.OnClickListener(){
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CountDown mCountDownUtil = CountDown.getCountDownTimer();
-                mCountDownUtil.setMillisInFuture(mTimerList.get(position).getTime());
-                mCountDownUtil.setCountDownInterval(1000);
-                mCountDownUtil.setTickDelegate(new CountDown.TickDelegate() {
-                    @Override
-                    public void onTick(long pMillisUntilFinished) {
-                        String minSec = mDateFormat.format(pMillisUntilFinished);
-                        holder.item_time.setTextColor(Color.RED);
-                        holder.item_time.setText(minSec);
-                    }
-                });
-                mCountDownUtil.setFinishDelegate(new CountDown.FinishDelegate() {
-                    @Override
-                    public void onFinish() {
-                        holder.item_time.setTextColor(Color.parseColor("#FFFFFF"));
-                        holder.item_time.setText("00:00");
-                        showDialog();
-                    }
-                });
-                notifyDataSetChanged();
-                mCountDownUtil.start();
-
+                if (mIsRunning) {
+                    mCountDownTimer.cancel();
+                    mIsRunning = false;
+                }
+                else {
+                    startTimer(holder, position);
+                }
             }
         });
     }
 
-    private void showDialog() {
+    // the following 2 methods are very important to keep the position right when scrolling
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    private void startTimer(ViewHolder holder, int position) {
+//        long mEndTime = System.currentTimeMillis() + mTimerList.get(position).getTime();
+
+        mCountDownTimer = new CountDownTimer(mTimerList.get(position).getTime(), 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                String minSec = mDateFormat.format(millisUntilFinished);
+                        holder.item_time.setTextColor(Color.RED);
+                        holder.item_time.setText(minSec);
+            }
+
+            @Override
+            public void onFinish() {
+                mIsRunning = false;
+                holder.item_time.setTextColor(Color.parseColor("#FFFFFF"));
+                holder.item_time.setText("00:00");
+                setOffNotification();
+            }
+        };
+        notifyDataSetChanged();
+        mCountDownTimer.start();
+
+        mIsRunning = true;
+    }
+
+
+    private void setOffNotification() {
         Vibrator vibrator = (Vibrator) mView.getContext().getSystemService(Context.VIBRATOR_SERVICE);
         long[] time = {0, 2000};
         vibrator.vibrate(time, -1);
@@ -110,9 +122,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return mTimerList.size();
     }
 
-//    public List<TimeInfo> getData() {
-//        return mTimerList;
-//    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView item_workout;
